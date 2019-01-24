d9d73d0b76b86c77c436d8b5ff6a892275b7a1e9
bhanu_agrawal <bhanu_agrawal@persistent.com>
2019-01-24 14:06:26 +0530

    println added in Sample class

--- src/main/java/com/psl/Sample.java
+++ src/main/java/com/psl/Sample.java
@@ -4,6 +4,7 @@ public class Sample {
     public static void main(String args[]){
         Sample s=new Sample();
         System.out.println(s.sumOfTwoNum(2,4));
+        System.out.println("Hello World");
     }
 
     public int sumOfTwoNum(int a,int b){
