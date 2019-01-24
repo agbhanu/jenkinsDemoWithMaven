79102e154aa9b6b59f3b67c8f80750e82f3885af
bhanu_agrawal <bhanu_agrawal@persistent.com>
2019-01-24 14:08:55 +0530

    changed

--- src/main/java/com/psl/Sample.java
+++ src/main/java/com/psl/Sample.java
@@ -1,13 +1,10 @@
 package com.psl;
 
 public class Sample {
-    public static void main(String args[]){
-        Sample s=new Sample();
-        System.out.println(s.sumOfTwoNum(2,4));
+    public static void main(String args[]) {
+        Sample s = new Sample();
+        System.out.println(s.sumOfTwoNum(2, 4));
         System.out.println("Hello World");
-    }
-
-    public int sumOfTwoNum(int a,int b){
-        return a+b;
+        System.out.println("Hello World");
     }
 }
